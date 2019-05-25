fabafb6d0278be64a81482ba1085f5632ae1c243
randombussinessboy <yangzy9846@outlook.com>
2019-05-25 19:17:33 +0800

    "修复了列号显示不准确的bug 为分析函数添加了更加完整的注释"

--- src/Compiler.java
+++ src/Compiler.java
@@ -4,24 +4,24 @@ import java.util.List;
 public class Compiler {
 
     public static void main(String[] args){
-        //为词法分析器设置需要分析的源码
-        Lex.setResource("src/simple_pascal_program_2.txt");
-
-        //请求词法分析器返回结果
-        List<Word> lexAnalysisResult=Lex.getResult();
-
-        if (Lex.isSuccessful()){
-            //这里开始进行语法分析
-        }
+//        //为词法分析器设置需要分析的源码
+//        Lex.setResource("src/simple_pascal_program_2.txt");
+//
+//        //请求词法分析器返回结果
+//        List<Word> lexAnalysisResult=Lex.getResult();
+//
+//        if (Lex.isSuccessful()){
+//            //这里开始进行语法分析
+//        }
 
         //分析另外一个源码文件,这里可以用循环进行实现
         Lex.setResource("src/simple_pascal_program_3.txt");
 
         //请求词法分析器返回结果
-       lexAnalysisResult=Lex.getResult();
+       List<Word> lexAnalysisResult=Lex.getResult();
         if (Lex.isSuccessful()){
             //这里开始进行语法分析
-            System.out.println("Prompt： 词法分析成功，开始进行语法分析");
+            System.out.println("Prompt： 词法分析成功，准备进行语法分析");
         }
     }
 }
--- src/Lex.java
+++ src/Lex.java
@@ -57,9 +57,20 @@ public class Lex {
     static  final int  LP=24;
     static  final int RP=25;
 
+      /*
+        -1 invalid character
+        -2 identifier is to long
+        -3 integer overflow
+        -4 invalid word
+         */
+    static final private int INVALIDCHARACTER=-1;
+    static final private int INDENTIFERTOLONG=-2;
+    static final private int INTEGEROVERFLOW=-3;
+    static final private int INVALIDWORD=-4;
+
 
     /*
-
+        分析结果,结构化的数据结构放入此数据结构
      */
     static private List analyzeResult=new ArrayList<Word>();
 
@@ -212,39 +223,60 @@ public class Lex {
 
     */
     static private void analyze(){
+
+        /*
+        相关变量的初始化，行为1，列为0
+        每递进一个字符column+1
+        字符串arr用来存放识别出来的单词
+        ch是从内存缓冲区读取出来的字符
+         */
         int row=1;
-        int clumn=1;
+        int column=0;
         String arr="";
         char ch;
         int category;
         for (int i=0;i<pointer;i++){
 
+            /*
+            循环开始，Pointer指针是源码文件总的字符个数
+
+             */
             ch=BUFFERSTRING[i];
+            column++;
             arr="";
             if (BUFFERSTRING[i]=='/'&&BUFFERSTRING[i+1]=='/'){
                 /*若为单行注释“//”，则去除后面的东西，直到遇到回车换行
                 限制i小于pointer 是为了防止源码文件的最后一行的单行注释检测不到
-                对应的\n
+                对应的\n从而发送数组越界
                  */
                 while (BUFFERSTRING[i]!='\n'&&i<=pointer){
-                    clumn++;
+                    /*
+                    I+1 column则加1
+                     */
 
                     i++;
+                    column++;
                 }
-                /*此处换行，应该行数加1，列数置为初值
-
+                /*
+                注意到while循环结束时此处为换行，则行数加1，列数置为初值
                  */
                 row++;
-                clumn=1;
-                //ch=BUFFERSTRING[i];ch的值为\n
+                column=0;
+                //结束本轮循环
                 continue;
             }
 
+
+            /*
+            下列循环是用来判断多行注释的
+             */
+
             if (BUFFERSTRING[i]=='/'&&BUFFERSTRING[i+1]=='*'){
                 i+=2;
-                clumn+=2;
+                column+=2;
                 while (BUFFERSTRING[i]!='*'||BUFFERSTRING[i+1]!='/'){
                     i++;
+                    column++;
                     if (i==pointer){
                         System.out.println("注释出错，" +
                                 "没有找到*/，编译终止");
@@ -252,44 +284,64 @@ public class Lex {
                     }
                     if (BUFFERSTRING[i]=='\n'){
                         row++;
-                        clumn=1;
+                        column=0;
 
                     }
-                    clumn++;
+
                 }
                 i+=2;
-                clumn+=2;
+                column+=2;
                 ch=BUFFERSTRING[i];
             }
 
+            /*
+            跳跃这些无意义的空格
+            换行则行数加1 列数置为处置
+             */
             if (ch==' '||ch=='\t'
             ||ch=='\n'||ch=='\r'){
                 if (ch=='\n'){
                     row++;
-                    clumn=1;
+                    column=0;
                 }
-                clumn++;
+
             }
+            /*
+            下面if的逻辑是
+            如果当前字符为大写字符
+            则向下试探进行组词，下一个字符
+            只要是大写字母或者数字就是合法的
+            如果不是无效字符则也可以加入arr
+            是因为，这样形成的一个单词叫做
+            非法单词 使用标志位flag来表示结果单词
+            中有没有无效的字符,无效的字符就是除了大写字母
+            ，数字以及我们用到的各种运算符分割符空白指标字符外的
+            字符
+            结果单词的字符大于8的时候应该进行截断，然后输出警告
+             */
             else if (isUpperLetter(ch)){
                 int flag=0;
-                int wordColumn=clumn;
+                int wordColumn=column;
                 while(isUpperLetter(ch)||isDigit(ch)||isInvalidCharacter(ch)){
                     arr+=ch;
                     if (isInvalidCharacter(ch))flag=1;
                     ch=BUFFERSTRING[++i];
-                    clumn++;
+                    column++;
                 }
                 if (flag==1){
-                    saveWordAndPrinter(-4,arr,row,wordColumn);
+                    saveWordAndPrinter(INVALIDWORD,arr,row,wordColumn);
                     i--;
-                    clumn--;
+                    column--;
                     continue;
                 }
                 if (arr.length()>=8){
-                    saveWordAndPrinter(-2,arr,row,wordColumn);
+                    saveWordAndPrinter(INDENTIFERTOLONG,arr,row,wordColumn);
                     arr=arr.substring(0,7);
                 }
                 i--;
+                column--;
+                //i-1 是因为当前字符不是这个单词的，所以我们回退，开始新的一轮
+                //大循环 把结果单词查保留字（关键字）表，看是关键字还是普通的标识符
                 category=searchReserve(arr);
                 if (category!=-1){
                     saveWordAndPrinter(category,arr,row,wordColumn);
@@ -298,99 +350,108 @@ public class Lex {
                    saveWordAndPrinter(ID,arr,row,wordColumn);
                 }
             }
+            //数字串的识别思路跟上面一样，这里添加了对小数的支持
             else if (isDigit(ch)){
-                int wordClumn=clumn;
-                while (isDigit(ch)||(ch=='.'&&isDigit(BUFFERSTRING[++i]))){
-                    if (ch=='.')i--;
+                int wordColumn=column;
+               while (isDigit(ch)||(ch=='.'&&isDigit(BUFFERSTRING[++i]))){
+                    if (ch=='.'){
+                        i--;
+                        column--;
+                    }
                     arr=arr+ch;
                       ch=BUFFERSTRING[++i];
-                      clumn++;
+                      column++;
                 }
                 i--;
-                clumn--;
+                column--;
+                //下面这个if是为检测数字打头的标识符，例如 12AB1 是非法的
                 if (isUpperLetter(BUFFERSTRING[i+1])){
                     i++;
-                    clumn++;
+                    column++;
                     while (isUpperLetter(BUFFERSTRING[i])||isDigit(BUFFERSTRING[i])){
                         arr=arr+BUFFERSTRING[i];
                         i++;
-                        clumn++;
+                        column++;
                     }
                     i--;
-                    clumn--;
-                    saveWordAndPrinter(-4,arr,row,wordClumn);
+                    column--;
+                    saveWordAndPrinter(INVALIDWORD,arr,row,wordColumn);
                     continue;
                 }
+                //如果这个数字串大于65535则应该报错，因为这样对这个数据结构来说越界了
                 int num=Integer.parseInt(arr);
                 if (num>65535){
-                    saveWordAndPrinter(-3,String.valueOf(num),row,wordClumn);
+                    saveWordAndPrinter(INTEGEROVERFLOW,String.valueOf(num),row,wordColumn);
                 }else {
-                    saveWordAndPrinter(INT,arr,row,wordClumn);
+                    saveWordAndPrinter(INT,arr,row,wordColumn);
                 }
 
-            }
+            }//下面就是简单CASE语句进行判断是什么字符了
             else switch (ch){
-                    case '+':saveWordAndPrinter(ADD,"+",row,clumn+1);clumn++;break;
-                    case '-':saveWordAndPrinter(18,"-",row,clumn+1);clumn++;break;
-                    case '*':saveWordAndPrinter(19,"*",row,clumn+1);clumn++;break;
-                    case '/':saveWordAndPrinter(20,"/",row,clumn+1);clumn++;break;
-                    case '=':saveWordAndPrinter(13,"=",row,clumn+1);clumn++;break;
+                    case '+':saveWordAndPrinter(ADD,"+",row,column);break;
+                    case '-':saveWordAndPrinter(SUB,"-",row,column);break;
+                    case '*':saveWordAndPrinter(MUL,"*",row,column);break;
+                    case '/':saveWordAndPrinter(DIV,"/",row,column);break;
+                    case '=':saveWordAndPrinter(EQ,"=",row,column);break;
                     //分界符
-                    case '(':saveWordAndPrinter(24,"(",row,clumn+1);clumn++;break;
-                    case ')':saveWordAndPrinter(25,")",row,clumn+1);clumn++;break;
-                    case ';':saveWordAndPrinter(22,";",row,clumn+1);clumn++;break;
-                    case ',':saveWordAndPrinter(23,",",row,clumn+1);clumn++;break;
+                    case '(':saveWordAndPrinter(LP,"(",row,column);break;
+                    case ')':saveWordAndPrinter(RP,")",row,column);break;
+                    case ';':saveWordAndPrinter(SEMI,";",row,column);break;
+                    case ',':saveWordAndPrinter(COMMA,",",row,column);break;
                     //运算符
                     case ':':{
-                        int wordClumn=clumn;
+                        int wordClumn=column;
                         ch = BUFFERSTRING[++i];
-                        clumn++;
-                        if(ch == '=')saveWordAndPrinter(21,":=",row,clumn);
+                        column++;
+                        if(ch == '=')saveWordAndPrinter(COL_EQ,":=",row,wordClumn);
                         else {
-                            saveWordAndPrinter(-1,":",row,clumn-1);
+                            saveWordAndPrinter(INVALIDCHARACTER,":",row,wordClumn);
                             i--;
-                            clumn--;
+                            column--;
                         }
                     }break;
                     case '>':{
+                        int wordCount=column;
                         ch = BUFFERSTRING[++i];
-                        clumn++;
-                        if(ch == '=')saveWordAndPrinter(16,">=",row,clumn);
+                        column++;
+                        if(ch == '=')saveWordAndPrinter(GE,">=",row,wordCount);
                         else {
-                            saveWordAndPrinter(15,">",row,clumn-1);
+                            saveWordAndPrinter(GT,">",row,wordCount);
                             i--;
-                            clumn--;
+                            column--;
                         }
                     }break;
                     case '<':{
+                        int wordCount=column;
                         ch = BUFFERSTRING[++i];
-                        clumn++;
+                        column++;
                         if(ch == '='){
-                            saveWordAndPrinter(12,"<=",row,clumn);
+                            saveWordAndPrinter(LE,"<=",row,wordCount);
 
                         }
                         else if (ch=='>'){
-                            saveWordAndPrinter(14,"<>",row,clumn);
+                            saveWordAndPrinter(NE,"<>",row,wordCount);
                         }
                         else {
-                            saveWordAndPrinter(11,"<",row,clumn-1);
+                            saveWordAndPrinter(LT,"<",row,wordCount);
                             i--;
-                            clumn--;
+                            column--;
                         }
                     }break;
 
                     //无识别
                     default:arr+=ch;
+                        int wordColumn=column;
                         i++;
-                        clumn++;
+                        column++;
                         while (isDigit(BUFFERSTRING[i])||isUpperLetter(BUFFERSTRING[i])){
                             arr+=BUFFERSTRING[i];
                             i++;
-                            clumn++;
+                            column++;
                         }
                         i--;
-                        clumn--;
-                        saveWordAndPrinter(-1,arr,row,clumn);
+                        column--;
+                        saveWordAndPrinter(INVALIDCHARACTER,arr,row,wordColumn);
 
                 }
         }
@@ -404,7 +465,12 @@ public class Lex {
     */
     static private void  saveWordAndPrinter(int category,String raw
     ,int wordRow ,int wordColumn){
-
+        /*
+        -1 invalid character
+        -2 identifier is to long
+        -3 integer overflow
+        -4 invalid word
+         */
        if (category==-1){
            System.out.println("Error: type"+"[非法字符]"+"  at line"+"["+wordRow+"]"+": ‘"+raw+"’含有非法字符");
            errorCount++;
